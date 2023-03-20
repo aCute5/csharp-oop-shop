b7bc66bedea0d1f38cfd359868d5f4f29f532a2b
Dario Costantini <costantini95dario@gmail.com>
2023-03-20 16:39:26 +0100

    adding array bonus 2

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -3,11 +3,24 @@ using csharp_oop_shop;
 using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
 
-Console.WriteLine("Hello, World!");
+
 var Microphone = new Product(name: "Microfono", description: "è comodo", price: 40);
+var Sgabelloo = new Product(name: "Sgabello", description: "è comodo", price: 20);
+var Telefono = new Product(name: "Telefono", description: "è potente", price: 120);
+
+Console.WriteLine("Hello, World!");
 Console.WriteLine(Microphone.Id);
 Console.WriteLine(Microphone.ZeroId());
 Console.WriteLine(Microphone.Id);
+
+Product[] arrProducts = new Product[10];
+
+
+arrProducts[0] = Microphone;
+arrProducts[1] = Telefono;
+arrProducts[2] = Sgabelloo;
+
+
 
 printProduct(Microphone);
 void printProduct(Product product)
